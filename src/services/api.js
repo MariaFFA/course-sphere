4f8c323e81d56ad52cbe3c58fd3251a3b4947e95
MariaFFA <mariaffa2003@gmail.com>
2025-10-23 20:51:54 -0300

    feat: Configura variável de ambiente de produção para a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
 
 // Função para autenticar o usuário
 export const apiLogin = async (email, password) => {
